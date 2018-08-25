f9ade59b11ce06ac5f2cf9beae25f578490aa67e
brenthaag <brenthaag@gmail.com>
2018-08-25 13:24:16 -0500

    Create airscape-whf.groovy

---
+++ devicetypes/brenthaag/airscape-whf.src/airscape-whf.groovy
@@ -0,0 +1,103 @@
+/**
+ *  AirScapeWHF
+ *
+ *  Copyright 2017 Brent Haag
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "AirScapeWHF", namespace: "brenthaag", author: "Brent Haag") {
+		capability "Actuator"
+		capability "Configuration"
+		capability "Indicator"
+		capability "Polling"
+		capability "Power Meter"
+		capability "Refresh"
+		capability "Sensor"
+		capability "Switch"
+		capability "Switch Level"
+		capability "Temperature Measurement"
+        
+        command "fanSpeedUp"
+        command "fanSpeedDn"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+    
+    preferences {
+           input name: "fanIPaddr", type: "text", title: "IP Address", description: "Enter IP address of fan", required: true
+           
+    }
+           
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'indicatorStatus' attribute
+	// TODO: handle 'power' attribute
+	// TODO: handle 'switch' attribute
+	// TODO: handle 'level' attribute
+	// TODO: handle 'temperature' attribute
+
+}
+
+// handle commands
+def configure() {
+	log.debug "Executing 'configure'"
+	// TODO: handle 'configure' command
+}
+
+def indicatorWhenOn() {
+	log.debug "Executing 'indicatorWhenOn'"
+	// TODO: handle 'indicatorWhenOn' command
+}
+
+def indicatorWhenOff() {
+	log.debug "Executing 'indicatorWhenOff'"
+	// TODO: handle 'indicatorWhenOff' command
+}
+
+def indicatorNever() {
+	log.debug "Executing 'indicatorNever'"
+	// TODO: handle 'indicatorNever' command
+}
+
+def poll() {
+	log.debug "Executing 'poll'"
+	// TODO: handle 'poll' command
+}
+
+def refresh() {
+	log.debug "Executing 'refresh'"
+	// TODO: handle 'refresh' command
+}
+
+def on() {
+	log.debug "Executing 'on'"
+	// TODO: handle 'on' command
+}
+
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def setLevel() {
+	log.debug "Executing 'setLevel'"
+	// TODO: handle 'setLevel' command
+}
