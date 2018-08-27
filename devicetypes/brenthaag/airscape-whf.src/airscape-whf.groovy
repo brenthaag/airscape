f61e7aa302b1d0dd3b8be48c22b22bcbf02670bc
brenthaag <brenthaag@gmail.com>
2018-08-26 22:07:57 -0500

    Update airscape-whf.groovy

--- devicetypes/brenthaag/airscape-whf.src/airscape-whf.groovy
+++ devicetypes/brenthaag/airscape-whf.src/airscape-whf.groovy
@@ -1,7 +1,7 @@
 /**
  *  AirScapeWHF
  *
- *  Copyright 2017 Brent Haag
+ *  Copyright 2018 Brent Haag
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
